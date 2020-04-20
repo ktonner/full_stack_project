c7ceede6ddb637e32b782393cd265c104aa9cd38
Yoom Davidson <ddavidson202020@gmail.com>
2020-04-20 12:02:04 -0400

    Add folders and files needed

---
+++ public/js/user.js
@@ -0,0 +1,7 @@
+const userName = document.querySelector(".userName");
+
+$.get("/auth/user")
+    .then(data => {
+        userName.textContent = data.email;
+    })
+    .catch(err => console.log(err));
