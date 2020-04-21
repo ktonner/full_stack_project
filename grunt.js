6fce5a7674f0b308492be026bf699a330367b23c
Kathleen Tonner <kathleentonner@gmail.com>
2020-04-21 17:40:18 -0400

    mor fiddling, some files added pls don't touch

---
+++ grunt.js
@@ -0,0 +1,18 @@
+//hello! I am a file used for Sass, please do not delete me :)
+// -Kathleen
+
+module.exports = function(grunt) {
+
+const grunt = require("grunt")
+
+grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+          loadPath: ['node_modules/foundation-sites/scss']
+        }
+      }
+    }
+  });
+
+  )
