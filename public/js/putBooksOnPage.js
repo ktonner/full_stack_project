274a1695a5a6e1c76b79e694bf0a483b5462ec0f
Yoom Davidson <ddavidson202020@gmail.com>
2020-04-22 12:54:46 -0400

    Request user info and book table

--- public/js/putBooksOnPage.js
+++ public/js/putBooksOnPage.js
@@ -1,25 +1,47 @@
-//const db = require("../models");
-//$(document).ready(function () {
-    // When the page loads, grab all of our books
-    $.get("/api/all", function (data) {
-       
-    
-        // if (data.length !== 0) {
 
-        //     for (var i = 0; i < data.length; i++) {
-
+$(document).ready(function () {
+        // Get the user information
+        $.ajax({
+                method: "GET",
+                url: "/auth/user"
+        }).then(function (user) {
+                // Put use information on the page
+                console.log("This should be", user)
                 var row = $("<div>");
-        //         //row.addClass("book");
-                row.append("Hello")
-        //         row.append("<p>" + data[i].title + "</p>");
-        //         row.append("<p>" + data[i].author + "</p>");
-        //         row.append ("<p>" + data[i].description + "</p>");   
-        //         row.append ("<p>" + data[i].pages + "</p>");  
-                $("#book").prepend(row);
+                
+                row.append("<p>" + user.id + "</p>");
+                row.append("<p>" + user.email + "</p>");
+                
+                $(".profile").append(row);
+                // Go get all the books for this user
+                getUserBooks(user.id);
+                
 
-        //     }
+        })
+        // Reuseable function to get all the user books
+        function getUserBooks(id) {
+        // Ask the server for the books from the table with this user
+        $.get("/api/all/" + id, function (data) {
+                console.log("Data should be here", data)
+                
+                if (data.length !== 0) {
 
-        // }
-  });
+                        for (var i = 0; i < data.length; i++) {
 
-//});
+                                var row = $("<div>");
+                                //row.addClass("book");
+                                row.append("Hello World")
+                                row.append("<p>" + data[i].title + "</p>");
+                                row.append("<p>" + data[i].author + "</p>");
+                                row.append("<p>" + data[i].description + "</p>");
+                                row.append("<p>" + data[i].pages + "</p>");
+                                $("#book").prepend(row);
+
+                        }
+
+                }
+        });
+}
+       
+
+});
