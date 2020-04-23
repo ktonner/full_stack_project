d446bf7dfbc3fda5d87b5359c3b7c3ac5f92d8bd
Yoom Davidson <ddavidson202020@gmail.com>
2020-04-23 13:01:02 -0400

    changes

--- public/js/saveBooksToDatabase.js
+++ public/js/saveBooksToDatabase.js
@@ -1,4 +1,3 @@
-
 $(document).on("click","#add", saveData);
 
 
@@ -9,7 +8,7 @@ function saveData(event) {
   var author1 = $(this).attr("data-author");
   var description1 = $(this).parent().siblings(".content").text().trim()
   var page1 =  $(this).attr("data-page");
-  
+  // Create newBook object to save
   var newBook = {
     title: title1,
     author: author1,
