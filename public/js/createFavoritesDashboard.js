77ed92fac253d3aefee0cc13e81ff44184c5baaf
James Pierson <jameswpbusiness@gmail.com>
2020-04-24 10:14:38 -0400

    favoriteslist style

--- public/js/createFavoritesDashboard.js
+++ public/js/createFavoritesDashboard.js
@@ -1,71 +1,95 @@
 $("#add").on("click", function () {
-console.log("clicked")
-        // var favCard = `
-        // <div class="card">
-        // <div class="card-content">
-        //       <div class="media">
-        //         <div class="media-left">
-        //           <figure class="image is-60x60">
-        //             <img src="${imgURL}" alt="Placeholder image">
-        //           </figure>
-        //         </div>
-        //         <div class="media-content">
-        //             <p class="title is-4">${title}</p>
-        //             <p class="subtitle is-6">${author}</p>
-        //             <p><button class="button" data-title="${title}" data-author="${author}" data-page="${pages}" id="add">Add favorite</button></p>
-        //             <br>
-        //             <div class="content">
-        //             ${desc} 
-        //             </div>
-        //           </div>
-        //       </div>
-        //   </div>
-        // </div>
+    console.log("clicked")
+    // var favCard = `
+    // <div class="card">
+    // <div class="card-content">
+    //       <div class="media">
+    //         <div class="media-left">
+    //           <figure class="image is-60x60">
+    //             <img src="${imgURL}" alt="Placeholder image">
+    //           </figure>
+    //         </div>
+    //         <div class="media-content">
+    //             <p class="title is-4">${title}</p>
+    //             <p class="subtitle is-6">${author}</p>
+    //             <p><button class="button" data-title="${title}" data-author="${author}" data-page="${pages}" id="add">Add favorite</button></p>
+    //             <br>
+    //             <div class="content">
+    //             ${desc} 
+    //             </div>
+    //           </div>
+    //       </div>
+    //   </div>
+    // </div>
 
-        // <hr>
-        // `
-
-
-        $.ajax({
-            method: "GET",
-            url: "/auth/user"
-        }).then(function (user) {
-            // Put use information on the page
-            console.log("This should be", user)
-            var row = $("<div>");
-
-            row.append("<p>" + user.id + "</p>");
-            row.append("<p>" + user.email + "</p>");
-
-            $(".profile").append(row);
-            // Go get all the books for this user
-            getUserBooks(user.id);
+    // <hr>
+    // `
 
 
-        })
-        // Reuseable function to get all the user books
-        function getUserBooks(id) {
-            // Ask the server for the books from the table with this user
-            $.get("/api/all/" + id, function (data) {
-                console.log("Data should be here", data)
+    $.ajax({
+        method: "GET",
+        url: "/auth/user"
+    }).then(function (user) {
+        // Put use information on the page
+        console.log("This should be", user)
+        var row = $("<div>");
 
-                if (data.length !== 0) {
+        row.append("<p>" + user.id + "</p>");
+        row.append("<p>" + user.email + "</p>");
 
-                    for (var i = 0; i < data.length; i++) {
+        $(".profile").append(row);
+        // Go get all the books for this user
+        getUserBooks(user.id);
 
-                        var row = $("<div>");
-                        //row.addClass("book");
-                        row.append("<p>" + data[i].title + "</p>");
-                        row.append("<p>" + data[i].author + "</p>");
-                        row.append("<p>" + data[i].description + "</p>");
-                        row.append("<p>" + data[i].pages + "</p>");
-                        $("#favorites").prepend(row);
-                        console.log("made it this far")
-                    }
 
+    })
+    // Reuseable function to get all the user books
+    function getUserBooks(id) {
+        // Ask the server for the books from the table with this user
+        $.get("/api/all/" + id, function (data) {
+            console.log("Data should be here", data)
+
+            if (data.length !== 0) {
+
+                for (var i = 0; i < data.length; i++) {
+
+                    // var row = $("<div>");
+                    // //row.addClass("book");
+                    // row.append("<p>" + data[i].title + "</p>");
+                    // row.append("<p>" + data[i].author + "</p>");
+                    // row.append("<p>" + data[i].description + "</p>");
+                    // row.append("<p>" + data[i].pages + "</p>");
+                    // $("#favorites").prepend(row);
+                    // console.log("made it this far")
+
+
+
+                    var favCard = `
+                    <div class="card">
+                    <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                    <figure class="image is-60x60">
+                         <img src="${data[i].imgURL}" alt="Placeholder image">
+                    </figure>
+                    </div>
+                    <div class="media-content">
+                      <p class="title is-4">${data[i].title}</p>
+                      <p class="subtitle is-6">${data[i].author}</p>
+                      <br>
+                    </div>
+                     </div>
+                    </div>
+                    </div>
+        
+                    <hr>
+          `
+                    $("#favorites").prepend(favCard)
                 }
-            });
-        }
+
+            }
+        });
     }
-})
+}
+)
 
