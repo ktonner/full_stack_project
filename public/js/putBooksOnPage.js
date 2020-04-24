457eb3e31d74062ff494831e759201081b0aa871
Yoom Davidson <ddavidson202020@gmail.com>
2020-04-24 10:58:31 -0400

    Fix the styling for profile page

--- public/js/putBooksOnPage.js
+++ public/js/putBooksOnPage.js
@@ -26,16 +26,13 @@ $(document).ready(function () {
 
                                 for (var i = 0; i < data.length; i++) {
                                         
-                                        var row = $("<div class='card'>");
+                                        var row = $("<div class='card'>").attr("style", "background-color: rgb(200, 228, 226);");
 
-                                        row.append("<p class='card-header-title'>" + data[i].title + "</p>");
-                                        row.append("<p class='author-content'>" + data[i].author + "</p>" + "<br>");
-                                        row.append("<p class='content'>" + data[i].description + "</p>");
-                                        row.append("<p class='content'>Number of pages: " + data[i].pages + "</p>" + "<hr>");
-                                        $(".card-header-title").attr("style", "font-size: 30px; margin-left: 15px;");
-                                        $(".author-content").attr("style", "color: hotpink; margin-left: 30px; font-weight: bold;");
-                                        $(".content").attr("style", "font-size: 20px; margin-left: 30px;");
-                                        $(".card").attr("style", "background-color: rgb(200, 228, 226);");
+                                        row.append("<p class='card-header-title' style='font-size: 30px; margin-left: 15px;'>" + data[i].title + "</p>");
+                                        row.append("<p class='author-content' style='color: hotpink; margin-left: 30px; font-weight: bold;'>" + data[i].author + "</p>" + "<br>");
+                                        row.append("<p class='content' style='font-size: 20px; margin-left: 30px;'>" + data[i].description + "</p>");
+                                        row.append("<p class='content' style='font-size: 20px; margin-left: 30px;'>Number of pages: " + data[i].pages + "</p>" + "<hr>");
+                                        
 
 
                                         $("#book").prepend(row);
